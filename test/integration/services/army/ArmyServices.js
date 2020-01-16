7b8233df5cd237b696d844571f83f9db2bfa3721
Zach Wild <zacharyjwild@gmail.com>
2020-01-16 17:37:01 -0500

    Update tests

--- test/integration/services/army/ArmyServices.js
+++ test/integration/services/army/ArmyServices.js
@@ -17,10 +17,20 @@ describe('ArmyServices', function() {
 
       let army_data = res.data;
       assert(army_data.length > 0);
-      // assertions for map data
+      // ensure that armies being returned have all the data expected
       for (let i = 0; i < army_data.length; i++) {
-        army_data[i].army_id.should.equal(i+1);
-        army_data[i].user_id.should.equal(i+1);
+        let army = army_data[i];
+        expect(army.army_id).to.be.not.null;
+        expect(army.user_id).to.be.not.null;
+        expect(army.army_units).to.be.not.null;
+        if (army.army_units.length > 0) {
+          for (let j = 0; j < army.army_units.length; j++) {
+            let units = army.army_units[j];
+            expect(units.army_units).to.be.not.null;
+            expect(units.unit_id).to.be.not.null;
+            expect(units.unit_count).to.be.not.null;
+          }
+        }
       }
 
     }).catch(err => {
--- test/unit/WorldServices.js
+++ test/unit/WorldServices.js
@@ -6,14 +6,10 @@ const assert  = chai.assert;
 // run all of the test on world services
 // ensure that messages are dispatched properly
 const WorldServices   = require('../../src/WorldServices');
-const UserServices    = require('../../src/services/UserServices');
 const Request         = require('../../src/Request');
 let worldServices     = new WorldServices();
 
 describe('WorldServices', function() {
-  beforeEach(function(done) {
-    worldServices = new WorldServices();
-  });
 
   it('should dispatch messages to the correct service', function() {
     let request = new Request();
